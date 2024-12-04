f1bf964b2c3b106bac2fba8cbfe8795756d438cc
WooRiGyeoRe <rufp0607@naver.com>
2024-12-04 13:37:48 +0900

    About 컴포넌트 수정

--- src/About.jsx
+++ src/About.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React from "react";
+import qs from "qs"; // qs 임포트
+import { useLocation } from "react-router-dom"; // useLocation 임포트
 
 const About = () => {
-    return (
-        <div>
-            <h1>소개</h1>
-            <p>path props를 배열로 설정해 사용합니다!</p>
-        </div>
-    );
+  const location = useLocation(); // useLocation 훅을 사용해 location 객체 가져오기
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true, // 문자열 맨 앞의 ?를 생략
+  });
+  const showDetail = query.detail === "true"; // 쿼리의 파싱 결과 값은 문자열
+  return (
+    <div>
+      <h1>소개</h1>
+      <p>이 프로젝트는 리액트 라우터 기초를 실습해 보는 예제 프로젝트입니다.</p>
+      {showDetail && <p>detail 값을 true로 설정하셨군요!</p>}
+    </div>
+  );
 };
 
 export default About;
